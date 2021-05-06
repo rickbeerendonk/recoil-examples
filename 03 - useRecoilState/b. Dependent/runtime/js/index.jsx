192d32c85e81804d6f2e522cfb892d69f786f386
Rick Beerendonk <rick@beerendonk.com>
2021-05-06 10:39:00 +0200

    Add "useRecoilState - Dependent" example.

---
+++ 03 - useRecoilState/b. Dependent/runtime/js/index.jsx
@@ -0,0 +1,43 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+/* global React, ReactDOM, Recoil */
+
+// Imports
+
+const { RecoilRoot, atom, useRecoilState } = Recoil;
+
+// Create Recoil Atom(s)
+
+const countState = atom({
+  key: 'countState', // unique ID (with respect to other atoms/selectors)
+  default: 0 // default value (aka initial value)
+});
+
+// React components
+
+function Counter() {
+  // Like useState() hook from React, only state is shareable between componenst
+  const [count, setCount] = useRecoilState(countState);
+
+  function handleClick() {
+    setCount(c => c + 1);
+  }
+
+  return (
+    <React.Fragment>
+      <button onClick={handleClick}>We have clicked {count} times.</button>
+    </React.Fragment>
+  );
+}
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Counter />
+      <Counter />
+    </RecoilRoot>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
