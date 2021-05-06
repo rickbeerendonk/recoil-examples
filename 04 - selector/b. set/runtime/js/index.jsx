452022e373a20235a53a11839fddcb0652d9c1fe
Rick Beerendonk <rick@beerendonk.com>
2021-05-06 14:42:03 +0200

    Add "Selector - set" example.

---
+++ 04 - selector/b. set/runtime/js/index.jsx
@@ -0,0 +1,72 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+/* global React, ReactDOM, Recoil */
+
+// Imports
+
+const { RecoilRoot, atom, selector, useRecoilState } = Recoil;
+
+// Create Recoil Atom(s)
+
+const nameState = atom({
+  key: 'nameState', // unique ID (with respect to other atoms/selectors)
+  default: 'Recoil' // default value (aka initial value)
+});
+
+// Create Recoil Selector(s)
+
+const nameFirstCharState = selector({
+  key: 'nameFirstCharState',
+  get: ({ get }) => {
+    return get(nameState)[0];
+  },
+  set: ({ get, set }, newValue) =>
+    set(nameState, newValue + get(nameState).substring(1))
+});
+
+// React components
+
+function GreetingEditor() {
+  // Like useState() hook from React, only state is shareable between componenst
+  const [name, setName] = useRecoilState(nameState);
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <React.Fragment>
+      <input autoFocus onChange={handleChange} value={name} />
+      <h1>Hello {name}!</h1>
+    </React.Fragment>
+  );
+}
+
+function GreetingFirstChar() {
+  const [firstChar, setFirstChar] = useRecoilState(nameFirstCharState);
+
+  function handleChange(e) {
+    setFirstChar(e.target.value);
+  }
+
+  return (
+    <input
+      style={{ color: 'red' }}
+      onChange={handleChange}
+      value={firstChar}
+      maxLength="1"
+    />
+  );
+}
+
+function App() {
+  return (
+    <RecoilRoot>
+      <GreetingEditor />
+      <GreetingFirstChar />
+    </RecoilRoot>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
