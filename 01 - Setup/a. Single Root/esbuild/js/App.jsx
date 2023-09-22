cd44de15fea23755cb9e30429d235a579a4219dc
Rick Beerendonk <rick@beerendonk.com>
2023-09-22 20:57:30 +0200

    Add ESBuild version.

---
+++ 01 - Setup/a. Single Root/esbuild/js/App.jsx
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { RecoilRoot } from 'recoil';
+
+import Greeting from './Greeting.jsx';
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Greeting />
+    </RecoilRoot>
+  );
+}
+
+export default App;
---
+++ 01 - Setup/a. Single Root/esbuild/ts/Greeting.tsx
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { useRecoilValue } from 'recoil';
+
+import { nameState } from './atoms.ts';
+
+function Greeting() {
+  const name = useRecoilValue(nameState);
+
+  return <h1>Hello {name}!</h1>;
+}
+
+export default Greeting;
